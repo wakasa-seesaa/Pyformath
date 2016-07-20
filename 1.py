ebf85862796c6284e7b10551c7ed1083a29521cf
wakasa-seesaa <wakasa@seesaa.co.jp>
2016-07-20 15:39:22 +0900

    refactor

--- 1.py
+++ 1.py
@@ -1,32 +1,24 @@
-# need take time to think...
-# Remember Divide-and-Conquer method as for top-down approach.
+# :p
 def even(n):
     if n is 0:
         exit()
     elif n % 2 is 1:
-        return 1
+        print "Odd"
     elif n % 2 is 0:
-        return 0
+        print "Even"
 
 def calc(n):
-    val = even(n)
-    
+    for i in range(1, 10):
+        n += 2
+        print(n)
 
 if __name__ == '__main__':
     x = float(input())
     if(x.is_integer() is False):
-        print('is not integer')
+        print('plz enter type of int')
     elif (int(x) is 0):
         print('do you know zero-divide?')
     else:
         y = int(x)
-        if (y % 2 is 0):
-            for i in range( 1, 10 ):
-                y += 2
-                print(y)
-            print('even')
-        if (y % 2 is not 0):
-            for i in range( 1, 10 ):
-                y += 2
-                print(y)
-            print('is not even')
+        calc(y)
+        even(y)
